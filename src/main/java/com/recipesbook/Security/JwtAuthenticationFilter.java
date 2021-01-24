20ebfcb505fa41a6a8178b1b6bf9a1aaa250aa66
OuniAbir <ouniabir118@gmail.com>
2021-01-24 01:59:48 +0100

    add refresh token - add expiry date to the JWT

--- src/main/java/com/recipesbook/Security/JwtAuthenticationFilter.java
+++ src/main/java/com/recipesbook/Security/JwtAuthenticationFilter.java
@@ -25,38 +25,40 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	private final JwtProvider jwtProvider;
 	private final UserDetailsService userDetailsService;
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
-		String jwt = getJwtFromRequest(request);
-
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String jwt = getJwtFromRequest(request);
 		/* if the token is valid , the request could be forwarded to the controller */
-		if (StringUtils.hasText(jwt) && jwtProvider.validateToken(jwt)) {
-			
-			/* now jwt is validated 
+        if (StringUtils.hasText(jwt) && jwtProvider.validateToken(jwt)) {
+        	/* now jwt is validated 
 			 * let's retreive the username from the jwt */
-			String username = jwtProvider.getUsernameFromJWT(jwt);	
-			
-			/* then the userdetail from the usename */
-			UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        	String username = jwtProvider.getUsernameFromJwt(jwt);
 
-			/* then store that user in the securitycontext */
-			UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails,null, userDetails.getAuthorities());
-			authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-			SecurityContextHolder.getContext().setAuthentication(authentication);
-		}
-		filterChain.doFilter(request, response);
-	}
+        	/* then the userdetail from the usename */
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            
+			/* then store that userDetails in the securitycontext */
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails,
+                    null, userDetails.getAuthorities());
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-	private String getJwtFromRequest(HttpServletRequest request) {
-	
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getJwtFromRequest(HttpServletRequest request) {
 		/* the token is sent as part of the auth header following the bearer schema */
-		String bearerToken = request.getHeader("Authorization");
+    	String bearerToken = request.getHeader("Authorization");
 
-		/* so let's get it by retriving the bearer token(jwt) from the HttpServletRequest*/ 
-		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-			return bearerToken.substring(7);
-		}
-		return bearerToken;
-	}
+    	/* so let's get it by retriving the bearer token(jwt) from the HttpServletRequest*/ 
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return bearerToken;
+    }
+
+
 }
