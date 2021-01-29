4caefffcef91b9e0c7fa79e81143033e2a030c0f
OuniAbir <ouniabir118@gmail.com>
2021-01-29 22:41:20 +0100

    deploy on herok - accept request from front

--- src/main/java/com/recipesbook/config/WebConfig.java
+++ src/main/java/com/recipesbook/config/WebConfig.java
@@ -9,10 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /* 
  * @EnableWebMvc in a spring-boot app will dismiss the autoconfiguration of spring boot 
  * so for the cross orgin origin support we just create a simple bean for configuration 
-
 @Configuration
 @EnableWebMvc 
-
 public class WebConfig implements WebMvcConfigurer {  */
 
 @Configuration
@@ -28,7 +26,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) 
             {
             	registry.addMapping("/**")
-            	.allowedOriginPatterns("https://book-recipe-angular-app.herokuapp.com")
+            	.allowedOriginPatterns("https://recipe-book-front-angular.herokuapp.com")
             	.allowedMethods("*")
             	.allowCredentials(true)
             	.allowedHeaders("*")
@@ -38,6 +36,3 @@ public class WebConfig {
     }
 	
 }
-
-
-
